427f813d4f9b3629689b85b88293bc16b225de37
MSA6706 <n.s.ariosam.guilan@gmail.com>
2025-07-28 13:29:19 +0330

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,11 @@
+
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.msa6706.aes256',
+  appName: 'AES256 Offline App',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
